2383a8b03ce6c8e1718cdf55f8d12b20a4357643
Vanessa Ponte <vanessaponte@gmail.com>
2023-07-15 16:21:33 -0300

    Função para checar numeros Função para capturar dados do input

--- src/index.js
+++ src/index.js
@@ -2,3 +2,38 @@ import validator from './validator.js';
 
 console.log(validator);
 
+
+let inputDoCartao = document.getElementById("cardNumber");
+
+
+function checkData() {
+
+    if (inputDoCartao.value == "") {
+        alert("Numero não informado");
+    } if (inputDoCartao.value.length > 0 && inputDoCartao.value.length < 16) {
+        alert("Este campo precisa ter 16 números");
+    } if (inputDoCartao.value.length == 16) {
+        console.log("Quantidade de números certa");
+    } else {
+        console.log("Números digitados");
+    }
+};
+
+// executa função quando o elemento perde o foco
+inputDoCartao.onblur = function () {
+
+    checkData();
+
+    let dadosDoCartao;
+    dadosDoCartao = document.getElementById("cardNumber").value;
+
+    console.log(dadosDoCartao);
+};
+
+
+
+
+
+
+
+
