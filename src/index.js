8479a0b5d836c342eeba446922fec5e9b44047f2
Vanessa Ponte <vanessaponte@gmail.com>
2023-07-17 11:28:44 -0300

    Adição de comentários sobre a manipulação da string

--- src/index.js
+++ src/index.js
@@ -8,27 +8,29 @@ console.log(validator);
 
 
 
-
+//acessar o elemento por ID e encapsular numa constante
 const inputDoCartao = document.getElementById("cardNumber");
 
-
+//disparar a função quando o input perder o focus
 inputDoCartao.onblur = function () {
 
     let numeroDoCartao;
 
     if (inputDoCartao.value.length == 16) {
         numeroDoCartao = inputDoCartao.value;
-
     } else {
         alert("Este campo precisa ter 16 números");
     };
 
-    // console.log(numeroDoCartao);
+    // conferir se a variável recebeu o value
+    console.log(numeroDoCartao);
 
-
+    // numeroDoCartao é um String
+    // para separar elementos de uma string e transformar numa array o método é split()
+    // mas para o método split é necessário um separador
+    // quero inserir um separador(virgula ou espaço) depois de cada elemento da string
 
     for (let i = 0; i <= 16; i++) {
-
 
     }
 
