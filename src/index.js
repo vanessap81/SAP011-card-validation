34aab2823f773be3bab0f965a8617253aaa28d9f
Vanessa Ponte <vanessaponte@gmail.com>
2023-07-18 16:03:53 -0300

    Funções revistas

--- src/index.js
+++ src/index.js
@@ -2,36 +2,22 @@ import validator from './validator.js';
 
 console.log(validator);
 
-
-
-
-
-
-
-//acessar o elemento por ID e encapsular numa constante
 const inputDoCartao = document.getElementById("cardNumber");
 
 function capturarInput() {
 
     let numeroDoCartao;
+    let re = /\d{16}/;
 
-    if (inputDoCartao.value.length == 16) {
+    if (inputDoCartao.value.length == 16 && re.test(inputDoCartao.value)) {
         numeroDoCartao = inputDoCartao.value;
+        console.log(numeroDoCartao);
     } else {
-        alert("Este campo precisa ter 16 números");
+        alert("Este campo deve conter 16 números");
     };
 
-    console.log(numeroDoCartao);
-
-    // numeroDoCartao é um String
-    // para separar elementos de uma string e transformar numa array o método é split()
-    // mas para o método split é necessário um separador
-    // quero inserir um separador(virgula ou espaço) depois de cada elemento da string
-
-    // for (let i = 0; i <= 16; i++) {
-    // }
+    validator.maskify(numeroDoCartao);
 };
 
-//disparar a função quando o input perder o focus
 inputDoCartao.addEventListener("blur", capturarInput);
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
 const validator = {
-  // ...
-};
+
+  isValid: function (creditCardNumber) {
+
+  },
+
+  maskify: function (creditCardNumber) {
+
+    //não esta funcionando
+    const regra = /\d/;
+    creditCardNumber.replace(regra, '#');
+    console.log(creditCardNumber);
+  }
+
+}
 
 export default validator;
