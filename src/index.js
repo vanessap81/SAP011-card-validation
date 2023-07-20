fae262d7d79daedbc32a4cef883916ce93fe55c3
Vanessa Ponte <vanessaponte@gmail.com>
2023-07-19 22:23:54 -0300

    adicionei métodos do validator

--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ const inputDoCartao = document.getElementById("cardNumber");
 
 function capturarInput() {
 
-    let numeroDoCartao;
-    let re = /\d{16}/;
+    // let numeroDoCartao;
+    // let re = /\d{16}/;
 
-    if (inputDoCartao.value.length == 16 && re.test(inputDoCartao.value)) {
-        numeroDoCartao = inputDoCartao.value;
-        console.log(numeroDoCartao);
-    } else {
-        alert("Este campo deve conter 16 números");
-    };
+    // if (inputDoCartao.value.length == 16 && re.test(inputDoCartao.value)) {
+    //     numeroDoCartao = inputDoCartao.value;
+    //     console.log(numeroDoCartao);
+    // } else {
+    //     alert("Este campo deve conter 16 números");
+    // };
 
-    validator.maskify(numeroDoCartao);
+    validator.isValid(inputDoCartao.value);
+    validator.maskify(inputDoCartao.value);
 };
 
 inputDoCartao.addEventListener("blur", capturarInput);
