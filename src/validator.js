d4df7599923477c9c3fd07c756a70c13c25853d1
Vanessa Ponte <vanessaponte@gmail.com>
2023-07-19 22:25:22 -0300

    metodos isValid e maskify alterados

--- src/validator.js
+++ src/validator.js
@@ -2,14 +2,26 @@ const validator = {
 
   isValid: function (creditCardNumber) {
 
+    let arrayCreditCardV = creditCardNumber.split('');
+    console.log(arrayCreditCardV);
+
+    arrayCreditCardV.reverse();
+    console.log(arrayCreditCardV);
   },
+
 
   maskify: function (creditCardNumber) {
 
-    //não esta funcionando
-    const regra = /\d/;
-    creditCardNumber.replace(regra, '#');
-    console.log(creditCardNumber);
+    let arrayCreditCard = creditCardNumber.split('');
+
+    let i;
+    let quantidade = arrayCreditCard.length;
+
+    for (i = 0; i < quantidade - 4; i++) {
+      arrayCreditCard.splice(i, 1, "#")
+    };
+    console.log(arrayCreditCard);
+
   }
 
 }
